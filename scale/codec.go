cbfeea6097f20edad0d69b80744193a3115f4fa6
vimukthi wickramasinghe <vimukthi@centrifuge.io>
2019-07-16 21:05:09 +0200

    fix io eof error

--- scale/codec.go
+++ scale/codec.go
@@ -336,7 +336,7 @@ func (pd Decoder) DecodeIntoReflectValue(target reflect.Value) error {
 		intHolder := reflect.New(t)
 		intPointer := intHolder.Interface()
 		err := binary.Read(pd.reader, binary.LittleEndian, intPointer)
-		if err != nil {
+		if err != nil && err != io.EOF {
 			return err
 		}
 		target.Set(intHolder.Elem())
