b42da07ffab22dc8dfd9b1acd11a32943c6ddfba
vimukthi wickramasinghe <vimukthi@centrifuge.io>
2019-07-30 16:11:00 +0200

    Pre-commit testing

--- test/main.go
+++ test/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"sync"
 	"sync/atomic"
@@ -16,6 +17,7 @@ import (
 
 const (
 	AnchorCommit = "anchor.commit"
+	AnchorPreCommit = "anchor.preCommit"
 	SubKeySign   = "sign-blob"
 
 	// Adjust below params according to your env + chain state + requirement
@@ -28,18 +30,53 @@ const (
 	Concurrency         = 1
 )
 
+type PreAnchorParams struct {
+	AnchorID 		 [32]byte
+	SigningRoot      [32]byte
+}
+
+func (a PreAnchorParams) Encode(encoder scale.Encoder) error {
+	encoder.Write(a.AnchorID[:])
+	encoder.Write(a.SigningRoot[:])
+	return nil
+}
+
 type AnchorParams struct {
 	AnchorIDPreimage [32]byte
 	DocRoot          [32]byte
 	Proof            [32]byte
 }
 
-func NewRandomAnchor() AnchorParams {
+func NewRandomAnchorParam() AnchorParams {
 	ap := AnchorParams{}
 	copy(ap.AnchorIDPreimage[:], utils.RandomSlice(32))
 	copy(ap.DocRoot[:], utils.RandomSlice(32))
 	copy(ap.Proof[:], utils.RandomSlice(32))
 	return ap
+}
+
+func NewRandomAnchorPreAnchorParams() (PreAnchorParams, AnchorParams){
+	pa := PreAnchorParams{}
+	ap := AnchorParams{}
+
+	// anchor id hashes
+	copy(ap.AnchorIDPreimage[:], utils.RandomSlice(32))
+	anchorID := blake2b.Sum256(ap.AnchorIDPreimage[:])
+	copy(pa.AnchorID[:], anchorID[:])
+
+	// root hashes
+	copy(pa.SigningRoot[:], utils.RandomSlice(32))
+	copy(ap.Proof[:], utils.RandomSlice(32))
+
+	var docRootPreimage []byte
+	if bytes.Compare(pa.SigningRoot[:], ap.Proof[:]) < 0  {
+		docRootPreimage = append(pa.SigningRoot[:], ap.Proof[:]...)
+	} else {
+		docRootPreimage = append(ap.Proof[:], pa.SigningRoot[:]...)
+	}
+
+	ap.DocRoot = blake2b.Sum256(docRootPreimage)
+	return pa, ap
 }
 
 func NewAnchorParamsFromHex(apre, docr, proof string) AnchorParams {
@@ -85,14 +122,14 @@ func (a *AnchorData) Decode(decoder scale.Decoder) error {
 	return nil
 }
 
-func Anchors(client substrate.Client, anchorIDPreImage []byte) (*AnchorData, error) {
+func Anchors(client substrate.Client, module string, fn string, anchorIDPreImage []byte) (*AnchorData, error) {
 	h := blake2b.Sum256(anchorIDPreImage)
 	m, err := client.MetaData(true)
 	if err != nil {
 		return nil, err
 	}
 
-	key, err := substrate.NewStorageKey(*m,"Anchor", "Anchors", h[:])
+	key, err := substrate.NewStorageKey(*m,module, fn, h[:])
 	if err != nil {
 		return nil, err
 	}
@@ -139,18 +176,37 @@ func main() {
 		go func() {
 			for i := 0; i < NumAnchorsPerThread; i++ {
 				// a := NewAnchorParamsFromHex("0x0000000000000000000000000000000000000000000000000000000000000901", "0x0000000000000000000000000000000000000000000000000000000000000000", "0x0000000000000000000000000000000000000000000000000000000000000000")
-				a := NewRandomAnchor()
-				aID := a.AnchorIDHex()
-				// fmt.Println("submitting new anchor with anchor ID", a.AnchorIDHex())
-				res, err := authRPC.SubmitExtrinsic(nonce, AnchorCommit, a)
+				pa, ap := NewRandomAnchorPreAnchorParams()
+				aID := ap.AnchorIDHex()
+				res, err := authRPC.SubmitExtrinsic(nonce, AnchorPreCommit, pa)
 				if err != nil {
-					fmt.Printf("FAIL!!! anchor ID %s failed with %s\n", aID, err.Error())
+					fmt.Printf("FAIL!!! pre commit for anchor ID %s failed with %s\n", aID, err.Error())
+					break
+				} else {
+					// verify pre anchor
+					for i := 0; i < 10; i++ {
+						time.Sleep(10 * time.Second)
+						anc, err := Anchors(client, "Anchor", "PreAnchors", ap.AnchorIDPreimage[:])
+						fmt.Println(err)
+						if anc != nil {
+							fmt.Printf("SUCCESS!!! pre anchor %v\n", anc)
+							break
+						}
+					}
+					fmt.Printf("SUCCESS!!! pre anchor ID %s , tx hash %s\n", aID, res)
+					atomic.AddUint64(&nonce, 1)
+				}
+
+				// fmt.Println("submitting new anchor with anchor ID", a.AnchorIDHex())
+				res, err = authRPC.SubmitExtrinsic(nonce, AnchorCommit, ap)
+				if err != nil {
+					fmt.Printf("FAIL!!! commit for anchor ID %s failed with %s\n", aID, err.Error())
 					break
 				} else {
 					// verify anchor
 					for i := 0; i < 10; i++ {
 						time.Sleep(10 * time.Second)
-						anc, err := Anchors(client, a.AnchorIDPreimage[:])
+						anc, err := Anchors(client, "Anchor", "Anchors", ap.AnchorIDPreimage[:])
 						fmt.Println(err)
 						if anc != nil {
 							fmt.Printf("SUCCESS!!! anchor %v\n", anc)
