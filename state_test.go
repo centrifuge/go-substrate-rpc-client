2c72d652e2c40fca0058c31a196911864539091c
vimukthi wickramasinghe <vimukthi@centrifuge.io>
2019-07-31 17:38:03 +0200

    TESTING RPC

--- state_test.go
+++ state_test.go
@@ -2,36 +2,51 @@ package substrate
 
 import (
 	"bytes"
-	"fmt"
 	"testing"
 
 	"github.com/centrifuge/go-substrate-rpc-client/scale"
+	"github.com/centrifuge/go-substrate-rpc-client/testrpc"
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/stretchr/testify/assert"
 )
 
+var testServer *testrpc.Server
+var testClient Client
+
+func TestMain(m *testing.M) {
+	testServer = new(testrpc.Server)
+	testServer.Init()
+	var err error
+	testClient, err = Connect("ws://localhost:8080")
+	if err != nil {
+		panic(err)
+	}
+	m.Run()
+	// TODO shutdown server
+}
+
 func TestState_GetMetaData(t *testing.T) {
-	s := State{nonetwork: true}
+	s := NewStateRPC(testClient)
 	res, err := s.MetaData([]byte{})
 	assert.NoError(t, err)
 	assert.Equal(t, "system", res.Metadata.Modules[0].Name)
-	// fmt.Println(res)
 }
 
 func TestState_Storage(t *testing.T) {
-	t.SkipNow()
-	c, _ := Connect("ws://127.0.0.1:9944")
-	s := NewStateRPC(c)
+	s := NewStateRPC(testClient)
 	b, _ := hexutil.Decode(AlicePubKey)
 	h, _ := hexutil.Decode("0x142d4b3d1946e4956b4bd5a5bfc906142e921b51415ceccb3c82b3bd3ff3daf1")
 
 	m, _ := s.MetaData(h)
-	key, _ := NewStorageKey(*m, "System", "AccountNonce", b)
-	res, _ := s.Storage(key, nil)
+	key, err := NewStorageKey(*m, "System", "AccountNonce", b)
+	assert.NoError(t, err)
+	res, err := s.Storage(key, nil)
+	assert.NoError(t, err)
 
 	buf := bytes.NewBuffer(res)
 	tempDec := scale.NewDecoder(buf)
 	var nonce uint64
-	tempDec.Decode(&nonce)
-	fmt.Println(nonce)
+	err = tempDec.Decode(&nonce)
+	assert.NoError(t, err)
+	assert.Equal(t, uint64(0xffffffffffffff1), nonce)
 }
---
+++ testrpc/server_test.go
@@ -0,0 +1,56 @@
+package testrpc
+
+import (
+	"github.com/centrifuge/go-centrifuge/utils"
+	"github.com/centrifuge/go-substrate-rpc-client"
+	"github.com/centrifuge/go-substrate-rpc-client/scale"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+const (
+	SubKeySign   = "sign-blob"
+
+	// SubKeyCmd subkey command to create signatures
+	SubKeyCmd = "/Users/vimukthi/.cargo/bin/subkey"
+)
+
+type AnchorParams struct {
+	AnchorIDPreimage [32]byte
+	DocRoot          [32]byte
+	Proof            [32]byte
+}
+
+func NewRandomAnchor() AnchorParams {
+	ap := AnchorParams{}
+	copy(ap.AnchorIDPreimage[:], utils.RandomSlice(32))
+	copy(ap.DocRoot[:], utils.RandomSlice(32))
+	copy(ap.Proof[:], utils.RandomSlice(32))
+	return ap
+}
+
+func (a *AnchorParams) Decode(decoder scale.Decoder) error {
+	decoder.Read(a.AnchorIDPreimage[:])
+	decoder.Read(a.DocRoot[:])
+	decoder.Read(a.Proof[:])
+	return nil
+}
+
+func (a AnchorParams) Encode(encoder scale.Encoder) error {
+	encoder.Write(a.AnchorIDPreimage[:])
+	encoder.Write(a.DocRoot[:])
+	encoder.Write(a.Proof[:])
+	return nil
+}
+
+func TestServer(t *testing.T) {
+	testServer := new(Server)
+	testServer.Init()
+	c, err := substrate.Connect("ws://127.0.0.1:8080")
+	assert.NoError(t, err)
+
+	a := substrate.NewAuthorRPC(c, utils.RandomSlice(32), SubKeyCmd, SubKeySign)
+	_, err = a.SubmitExtrinsic(1, "anchor.commit", NewRandomAnchor())
+	assert.NoError(t, err)
+
+}
