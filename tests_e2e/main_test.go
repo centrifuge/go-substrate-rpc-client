0afd6dbdaf50d7c09851998574db97e82fdfcb38
Philip Stanislaus <6912756+philipstanislaus@users.noreply.github.com>
2019-09-18 16:20:32 +0200

    Adjust nolint config

--- tests_e2e/main_test.go
+++ tests_e2e/main_test.go
@@ -16,7 +16,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-package tests_e2e //nolint:golint
+package tests_e2e //nolint:stylecheck,golint
 
 import (
 	"fmt"
