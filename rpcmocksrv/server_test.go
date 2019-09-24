c0c7caf8e82c6ca1c0607c1b2aae71584ae010f9
Philip Stanislaus <6912756+philipstanislaus@users.noreply.github.com>
2019-09-24 21:59:38 +0200

    Remove comments

--- rpcmocksrv/server_test.go
+++ rpcmocksrv/server_test.go
@@ -7,41 +7,6 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-// const (
-// 	SubKeySign = "sign-blob"
-
-// 	// SubKeyCmd subkey command to create signatures
-// 	SubKeyCmd = "/Users/vimukthi/.cargo/bin/subkey"
-// )
-
-// type AnchorParams struct {
-// 	AnchorIDPreimage [32]byte
-// 	DocRoot          [32]byte
-// 	Proof            [32]byte
-// }
-
-// func NewRandomAnchor() AnchorParams {
-// 	ap := AnchorParams{}
-// 	copy(ap.AnchorIDPreimage[:], utils.RandomSlice(32))
-// 	copy(ap.DocRoot[:], utils.RandomSlice(32))
-// 	copy(ap.Proof[:], utils.RandomSlice(32))
-// 	return ap
-// }
-
-// func (a *AnchorParams) Decode(decoder scale.Decoder) error {
-// 	decoder.Read(a.AnchorIDPreimage[:])
-// 	decoder.Read(a.DocRoot[:])
-// 	decoder.Read(a.Proof[:])
-// 	return nil
-// }
-
-// func (a AnchorParams) Encode(encoder scale.Encoder) error {
-// 	encoder.Write(a.AnchorIDPreimage[:])
-// 	encoder.Write(a.DocRoot[:])
-// 	encoder.Write(a.Proof[:])
-// 	return nil
-// }
-
 type TestService struct {
 }
 
