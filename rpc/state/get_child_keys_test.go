6fb88e387af6ba9189900e4bb7a5e98c30dd9268
philipstanislaus <6912756+philipstanislaus@users.noreply.github.com>
2019-10-21 21:45:23 +0200

    Add tests for child storage functionality (#30)
    * Add tests for child storage functionality

---
+++ rpc/state/get_child_keys_test.go
@@ -0,0 +1,38 @@
+// Go Substrate RPC Client (GSRPC) provides APIs and types around Polkadot and any Substrate-based chain RPC calls
+//
+// Copyright 2019 Centrifuge GmbH
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package state
+
+import (
+	"testing"
+
+	"github.com/centrifuge/go-substrate-rpc-client/types"
+	"github.com/stretchr/testify/assert"
+)
+
+var prefix = types.NewStorageKey(types.MustHexDecodeString(mockSrv.childStorageTrieKeyHex))[:8]
+
+func TestState_GetChildKeysLatest(t *testing.T) {
+	keys, err := state.GetChildKeysLatest(childStorageKey, prefix)
+	assert.NoError(t, err)
+	assert.Equal(t, []types.StorageKey{types.MustHexDecodeString(mockSrv.childStorageTrieKeyHex)}, keys)
+}
+
+func TestState_GetChildKeys(t *testing.T) {
+	keys, err := state.GetChildKeys(childStorageKey, prefix, mockSrv.blockHashLatest)
+	assert.NoError(t, err)
+	assert.Equal(t, []types.StorageKey{types.MustHexDecodeString(mockSrv.childStorageTrieKeyHex)}, keys)
+}
---
+++ rpc/state/get_child_storage_test.go
@@ -0,0 +1,53 @@
+// Go Substrate RPC Client (GSRPC) provides APIs and types around Polkadot and any Substrate-based chain RPC calls
+//
+// Copyright 2019 Centrifuge GmbH
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package state
+
+import (
+	"testing"
+
+	"github.com/centrifuge/go-substrate-rpc-client/types"
+	"github.com/stretchr/testify/assert"
+)
+
+var childStorageKey = types.NewStorageKey(types.MustHexDecodeString(mockSrv.childStorageKeyHex))
+var key = types.NewStorageKey(types.MustHexDecodeString(mockSrv.childStorageTrieKeyHex))
+
+func TestState_GetChildStorageLatest(t *testing.T) {
+	var decoded ChildStorageTrieTestVal
+	err := state.GetChildStorageLatest(childStorageKey, key, &decoded)
+	assert.NoError(t, err)
+	assert.Equal(t, mockSrv.childStorageTrieValue, decoded)
+}
+
+func TestState_GetChildStorage(t *testing.T) {
+	var decoded ChildStorageTrieTestVal
+	err := state.GetChildStorageLatest(childStorageKey, key, &decoded)
+	assert.NoError(t, err)
+	assert.Equal(t, mockSrv.childStorageTrieValue, decoded, mockSrv.blockHashLatest)
+}
+
+func TestState_GetChildStorageRawLatest(t *testing.T) {
+	data, err := state.GetChildStorageRawLatest(childStorageKey, key)
+	assert.NoError(t, err)
+	assert.Equal(t, mockSrv.childStorageTrieValueHex, data.Hex())
+}
+
+func TestState_GetChildStorageRaw(t *testing.T) {
+	data, err := state.GetChildStorageRaw(childStorageKey, key, mockSrv.blockHashLatest)
+	assert.NoError(t, err)
+	assert.Equal(t, mockSrv.childStorageTrieValueHex, data.Hex())
+}
