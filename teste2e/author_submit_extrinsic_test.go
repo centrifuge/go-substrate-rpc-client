8a3b02987cecfaf9458d7892f5f493c370977e73
Vincent Geddes <vincent.geddes@hey.com>
2020-07-25 03:59:47 +0200

    Support extrinsic payload v4

--- teste2e/author_submit_extrinsic_test.go
+++ teste2e/author_submit_extrinsic_test.go
@@ -176,3 +176,98 @@ func TestChain_SubmitExtrinsic(t *testing.T) {
 		time.Sleep(1 * time.Second)
 	}
 }
+
+func TestChain_SubmitExtrinsic_Simple(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping end-to-end test in short mode.")
+	}
+
+	from, ok := signature.LoadKeyringPairFromEnv()
+	if !ok {
+		t.Skip("skipping end-to-end that requires a private key because TEST_PRIV_KEY is not set or empty")
+	}
+
+	api, err := gsrpc.NewSubstrateAPI(config.Default().RPCURL)
+	if err != nil {
+		panic(err)
+	}
+
+	meta, err := api.RPC.State.GetMetadataLatest()
+	if err != nil {
+		panic(err)
+	}
+
+	bob, err := types.NewAddressFromHexAccountID("0x8eaf04151687736326c9fea17e25fc5287613693c912909cb226aa4794f26a48")
+	if err != nil {
+		panic(err)
+	}
+
+	c, err := types.NewCall(meta, "Balances.transfer", bob, types.NewUCompactFromUInt(6969))
+	if err != nil {
+		panic(err)
+	}
+
+	ext := types.NewExtrinsic(c)
+
+	era := types.ExtrinsicEra{IsMortalEra: false}
+
+	genesisHash, err := api.RPC.Chain.GetBlockHash(0)
+	if err != nil {
+		panic(err)
+	}
+
+	rv, err := api.RPC.State.GetRuntimeVersionLatest()
+	if err != nil {
+		panic(err)
+	}
+
+	key, err := types.CreateStorageKey(meta, "System", "Account", from.PublicKey, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	var accountInfo types.AccountInfo
+	ok, err = api.RPC.State.GetStorageLatest(key, &accountInfo)
+	if err != nil || !ok {
+		panic(err)
+	}
+
+	nonce := uint32(accountInfo.Nonce)
+
+	o := types.SignatureOptions{
+		BlockHash:   genesisHash,
+		Era:         era,
+		GenesisHash: genesisHash,
+		Nonce:       types.NewUCompactFromUInt(uint64(nonce)),
+		SpecVersion: rv.SpecVersion,
+		TxVersion:   1,
+		Tip:         types.NewUCompactFromUInt(0),
+	}
+
+	extI := ext
+
+	err = extI.Sign(from, o)
+	if err != nil {
+		panic(err)
+	}
+
+	extEnc, err := types.EncodeToHexString(extI)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Extrinsic: %#v\n", extEnc)
+
+	_, err = api.RPC.Author.SubmitExtrinsic(extI)
+	if err != nil {
+		panic(err)
+	}
+
+	xts, err := api.RPC.Author.PendingExtrinsics()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Pending extrinsics: %#v\n", xts)
+
+}
