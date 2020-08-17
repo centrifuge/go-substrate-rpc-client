ff085ba46ae9459d50cde2e8077a5855f0caa04e
Rob De Feo <robdefeo@gmail.com>
2020-08-17 23:10:53 +0100

    nonce to be uint32

--- rpc/system/account.go
+++ rpc/system/account.go
@@ -2,8 +2,8 @@ package system
 
 import "github.com/mailchain/go-substrate-rpc-client/types"
 
-func (c *System) AccountNextIndex(address string) (types.U64, error) {
-	var res types.U64
+func (c *System) AccountNextIndex(address string) (types.U32, error) {
+	var res types.U32
 
 	if err := c.client.Call(&res, "system_accountNextIndex", address); err != nil {
 		return 0, err
