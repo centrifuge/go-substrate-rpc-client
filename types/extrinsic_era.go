1f5f2d8e7822b534865c1459353f93ab93fab2e9
Vincent Geddes <vincent@snowfork.com>
2021-01-18 18:27:01 +0200

    Revert "Update ExtrinsicEra type"
    This reverts commit f56aad0b8041780a917ff39582ba5f1e2d5fb8f5.

--- types/extrinsic_era.go
+++ types/extrinsic_era.go
@@ -17,8 +17,6 @@
 package types
 
 import (
-	"fmt"
-
 	"github.com/centrifuge/go-substrate-rpc-client/v2/scale"
 )
 
@@ -30,66 +28,49 @@ type ExtrinsicEra struct {
 	AsMortalEra MortalEra
 }
 
-// MortalEra for an extrinsic, indicating period and phase
-type MortalEra struct {
-	First  U64
-	Second U64
-}
-
 func (e *ExtrinsicEra) Decode(decoder scale.Decoder) error {
-	tag, err := decoder.ReadOneByte()
+	first, err := decoder.ReadOneByte()
 	if err != nil {
 		return err
 	}
 
-	switch tag {
-	case 0:
+	if first == 0 {
 		e.IsImmortalEra = true
-	case 1:
-		e.IsMortalEra = true
-
-		err = decoder.Decode(&e.AsMortalEra.First)
-		if err != nil {
-			return err
-		}
-
-		err = decoder.Decode(&e.AsMortalEra.Second)
-		if err != nil {
-			return err
-		}
-	default:
-		return fmt.Errorf("No such variant for ExtrinsicEra")
+		return nil
 	}
+
+	e.IsMortalEra = true
+
+	second, err := decoder.ReadOneByte()
+	if err != nil {
+		return err
+	}
+
+	e.AsMortalEra = MortalEra{first, second}
 
 	return nil
 }
 
 func (e ExtrinsicEra) Encode(encoder scale.Encoder) error {
-	var err error
-	switch {
-	case e.IsImmortalEra:
-		err = encoder.PushByte(0)
-		if err != nil {
-			return err
-		}
-	case e.IsMortalEra:
-		err = encoder.PushByte(1)
-		if err != nil {
-			return err
-		}
+	if e.IsImmortalEra {
+		return encoder.PushByte(0)
+	}
 
-		err = encoder.Encode(e.AsMortalEra.First)
-		if err != nil {
-			return err
-		}
+	err := encoder.PushByte(e.AsMortalEra.First)
+	if err != nil {
+		return err
+	}
 
-		err = encoder.Encode(e.AsMortalEra.Second)
-		if err != nil {
-			return err
-		}
-	default:
-		return fmt.Errorf("No such variant for ExtrinsicEra")
+	err = encoder.PushByte(e.AsMortalEra.Second)
+	if err != nil {
+		return err
 	}
 
 	return nil
+}
+
+// MortalEra for an extrinsic, indicating period and phase
+type MortalEra struct {
+	First  byte
+	Second byte
 }
