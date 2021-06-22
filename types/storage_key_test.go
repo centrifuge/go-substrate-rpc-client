22fe7a9f79a9fba6fdc83d2a50dd6879c6ac575e
Vincent Geddes <vincent.geddes@hey.com>
2021-06-22 12:36:29 +0200

    Update module paths

--- types/storage_key_test.go
+++ types/storage_key_test.go
@@ -18,8 +18,8 @@ package types_test
 
 import (
 	"encoding/binary"
-	"github.com/centrifuge/go-substrate-rpc-client/v3/hash"
-	"github.com/centrifuge/go-substrate-rpc-client/v3/xxhash"
+	"github.com/snowfork/go-substrate-rpc-client/v3/hash"
+	"github.com/snowfork/go-substrate-rpc-client/v3/xxhash"
 	"strings"
 	"testing"
 
