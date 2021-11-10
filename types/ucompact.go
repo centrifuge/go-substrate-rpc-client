061050e40a8222a57e548167de10869f86671847
Gkirito <Gkirito@users.noreply.github.com>
2021-11-10 20:37:38 +0800

    Supplementary code based on the current metadata v14 code (#209)
    * ✨ feat(types/metadataV14.go): Add missing field Type
    
    * ✨ feat(types/ucompact.go & metadataV14_portable.go): Combine similar struct
    
    * 📖 docs(types/metadataV14_portable.go): delete the commented code

--- types/ucompact.go
+++ types/ucompact.go
@@ -28,6 +28,11 @@ func NewUCompact(value *big.Int) UCompact {
 	return UCompact(*value)
 }
 
+func (u *UCompact) Int64() int64 {
+	i := big.Int(*u)
+	return i.Int64()
+}
+
 func NewUCompactFromUInt(value uint64) UCompact {
 	return NewUCompact(new(big.Int).SetUint64(value))
 }
