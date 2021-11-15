adad192b41aa4a3d3793c6c3c6ac9d750dbb0221
Nuno Alexandre <hi@nunoalexandre.com>
2021-11-15 09:52:48 +0100

    Bump substrate Docker image version
    We bump the substrate docker image to the latest version to date,
    running a chain with metadata v14.

--- main_test.go
+++ main_test.go
@@ -50,7 +50,7 @@ func Example_simpleConnect() {
 
 	fmt.Printf("You are connected to chain %v using %v v%v\n", chain, nodeName, nodeVersion)
 
-	// Output: You are connected to chain Development using Substrate Node v2.0.0-49a4103f4b-x86_64-linux-gnu
+	// Output: You are connected to chain Development using Substrate Node v3.0.0-dev-b01bd31-x86_64-linux-gnu
 }
 
 func Example_listenToNewBlocks() {
