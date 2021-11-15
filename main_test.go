7e89aa9343856be6156058b28a83b3af8365e9ab
Nuno Alexandre <hi@nunoalexandre.com>
2021-11-15 17:08:10 +0100

    try: Re-enable Test_makeASimpleTransfer with Tip
    With this setup I have the tests green 7/7 runs locally.

--- main_test.go
+++ main_test.go
@@ -182,87 +182,87 @@ func Example_unsubscribeFromListeningToUpdates() {
 	}
 }
 
-// func Example_makeASimpleTransfer() {
-// 	// This sample shows how to create a transaction to make a transfer from one an account to another.
+func Example_makeASimpleTransfer() {
+	// This sample shows how to create a transaction to make a transfer from one an account to another.
 
-// 	// Instantiate the API
-// 	api, err := gsrpc.NewSubstrateAPI(config.Default().RPCURL)
-// 	if err != nil {
-// 		panic(err)
-// 	}
+	// Instantiate the API
+	api, err := gsrpc.NewSubstrateAPI(config.Default().RPCURL)
+	if err != nil {
+		panic(err)
+	}
 
-// 	meta, err := api.RPC.State.GetMetadataLatest()
-// 	if err != nil {
-// 		panic(err)
-// 	}
+	meta, err := api.RPC.State.GetMetadataLatest()
+	if err != nil {
+		panic(err)
+	}
 
-// 	// Create a call, transferring 12345 units to Bob
-// 	bob, err := types.NewMultiAddressFromHexAccountID("0x8eaf04151687736326c9fea17e25fc5287613693c912909cb226aa4794f26a48")
-// 	if err != nil {
-// 		panic(err)
-// 	}
+	// Create a call, transferring 12345 units to Bob
+	bob, err := types.NewMultiAddressFromHexAccountID("0x8eaf04151687736326c9fea17e25fc5287613693c912909cb226aa4794f26a48")
+	if err != nil {
+		panic(err)
+	}
 
-// 	// 1 unit of transfer
-// 	bal, ok := new(big.Int).SetString("100000000000000", 10)
-// 	if !ok {
-// 		panic(fmt.Errorf("failed to convert balance"))
-// 	}
+	// 1 unit of transfer
+	bal, ok := new(big.Int).SetString("100000000000000", 10)
+	if !ok {
+		panic(fmt.Errorf("failed to convert balance"))
+	}
 
-// 	c, err := types.NewCall(meta, "Balances.transfer", bob, types.NewUCompact(bal))
-// 	if err != nil {
-// 		panic(err)
-// 	}
+	c, err := types.NewCall(meta, "Balances.transfer", bob, types.NewUCompact(bal))
+	if err != nil {
+		panic(err)
+	}
 
-// 	// Create the extrinsic
-// 	ext := types.NewExtrinsic(c)
+	// Create the extrinsic
+	ext := types.NewExtrinsic(c)
 
-// 	genesisHash, err := api.RPC.Chain.GetBlockHash(0)
-// 	if err != nil {
-// 		panic(err)
-// 	}
+	genesisHash, err := api.RPC.Chain.GetBlockHash(0)
+	if err != nil {
+		panic(err)
+	}
 
-// 	rv, err := api.RPC.State.GetRuntimeVersionLatest()
-// 	if err != nil {
-// 		panic(err)
-// 	}
+	rv, err := api.RPC.State.GetRuntimeVersionLatest()
+	if err != nil {
+		panic(err)
+	}
 
-// 	key, err := types.CreateStorageKey(meta, "System", "Account", signature.TestKeyringPairAlice.PublicKey)
-// 	if err != nil {
-// 		panic(err)
-// 	}
+	key, err := types.CreateStorageKey(meta, "System", "Account", signature.TestKeyringPairAlice.PublicKey)
+	if err != nil {
+		panic(err)
+	}
 
-// 	var accountInfo types.AccountInfo
-// 	ok, err = api.RPC.State.GetStorageLatest(key, &accountInfo)
-// 	if err != nil || !ok {
-// 		panic(err)
-// 	}
+	var accountInfo types.AccountInfo
+	ok, err = api.RPC.State.GetStorageLatest(key, &accountInfo)
+	if err != nil || !ok {
+		panic(err)
+	}
 
-// 	nonce := uint32(accountInfo.Nonce)
-// 	o := types.SignatureOptions{
-// 		BlockHash:          genesisHash,
-// 		Era:                types.ExtrinsicEra{IsMortalEra: false},
-// 		GenesisHash:        genesisHash,
-// 		Nonce:              types.NewUCompactFromUInt(uint64(nonce)),
-// 		SpecVersion:        rv.SpecVersion,
-// 		Tip:                types.NewUCompactFromUInt(100),
-// 		TransactionVersion: rv.TransactionVersion,
-// 	}
+	nonce := uint32(accountInfo.Nonce)
+	o := types.SignatureOptions{
+		BlockHash:          genesisHash,
+		Era:                types.ExtrinsicEra{IsMortalEra: false},
+		GenesisHash:        genesisHash,
+		Nonce:              types.NewUCompactFromUInt(uint64(nonce)),
+		SpecVersion:        rv.SpecVersion,
+		Tip:                types.NewUCompactFromUInt(100),
+		TransactionVersion: rv.TransactionVersion,
+	}
 
-// 	// Sign the transaction using Alice's default account
-// 	err = ext.Sign(signature.TestKeyringPairAlice, o)
-// 	if err != nil {
-// 		panic(err)
-// 	}
+	// Sign the transaction using Alice's default account
+	err = ext.Sign(signature.TestKeyringPairAlice, o)
+	if err != nil {
+		panic(err)
+	}
 
-// 	// Send the extrinsic
-// 	_, err = api.RPC.Author.SubmitExtrinsic(ext)
-// 	if err != nil {
-// 		panic(err)
-// 	}
+	// Send the extrinsic
+	_, err = api.RPC.Author.SubmitExtrinsic(ext)
+	if err != nil {
+		panic(err)
+	}
 
-// 	fmt.Printf("Balance transferred from Alice to Bob: %v\n", bal.String())
-// 	// Output: Balance transferred from Alice to Bob: 100000000000000
-// }
+	fmt.Printf("Balance transferred from Alice to Bob: %v\n", bal.String())
+	// Output: Balance transferred from Alice to Bob: 100000000000000
+}
 
 func Example_displaySystemEvents() {
 	// Query the system events and extract information from them. This example runs until exited via Ctrl-C
