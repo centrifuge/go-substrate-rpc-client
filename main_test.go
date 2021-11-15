754232bb28dbf815edd392352c6ebb42692befcb
Nuno Alexandre <hi@nunoalexandre.com>
2021-11-15 12:50:26 +0100

    try: Disable tests -race flag

--- main_test.go
+++ main_test.go
@@ -49,8 +49,7 @@ func Example_simpleConnect() {
 	}
 
 	fmt.Printf("You are connected to chain %v using %v v%v\n", chain, nodeName, nodeVersion)
-
-	// Output: You are connected to chain Development using Substrate Node v3.0.0-dev-b01bd31-x86_64-linux-gnu
+	// Output: You are connected to chain Development using Substrate Node v3.0.0-dev-1b646b2-x86_64-linux-gnu
 }
 
 func Example_listenToNewBlocks() {
