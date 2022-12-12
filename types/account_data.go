158963185194e819edbfa78612b76398ee45df8e
cdamian <17934949+cdamian@users.noreply.github.com>
2022-12-12 12:14:18 +0200

    types: Add Sufficients to AccountInfo

--- types/account_data.go
+++ types/account_data.go
@@ -18,10 +18,11 @@ package types
 
 // AccountInfo contains information of an account
 type AccountInfo struct {
-	Nonce     U32
-	Consumers U32
-	Providers U32
-	Data      struct {
+	Nonce       U32
+	Consumers   U32
+	Providers   U32
+	Sufficients U32
+	Data        struct {
 		Free       U128
 		Reserved   U128
 		MiscFrozen U128
