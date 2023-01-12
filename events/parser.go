46fbe23989fc9eb4890d0239bf8269c2315a172a
cdamian <17934949+cdamian@users.noreply.github.com>
2023-01-12 19:45:56 +0200

    events: Extract decode type def

--- events/parser.go
+++ events/parser.go
@@ -2,7 +2,6 @@ package events
 
 import (
 	"bytes"
-	"encoding/json"
 	"errors"
 	"fmt"
 
@@ -16,16 +15,6 @@ type Event struct {
 	Phase  *types.Phase
 	Topics []types.Hash
 }
-
-type OverrideDecodeFn func(decoder *scale.Decoder) (any, error)
-
-var (
-	overrideDecodeFnMap = map[string]OverrideDecodeFn{
-		//"ref_time": func(decoder *scale.Decoder) (any, error) {
-		//	return decode[types.UCompact](decoder)
-		//},
-	}
-)
 
 func ParseEvents(meta *types.Metadata, sd *types.StorageDataRaw) ([]*Event, error) {
 	decoder := scale.NewDecoder(bytes.NewReader(*sd))
@@ -138,135 +127,288 @@ func parseFields(meta *types.Metadata, decoder *scale.Decoder, fields []types.Si
 
 		fieldName := string(field.Name)
 
-		if overrideDecodeFn, ok := overrideDecodeFnMap[fieldName]; ok {
-			fieldValue, err := overrideDecodeFn(decoder)
-
-			if err != nil {
-				return nil, fmt.Errorf("couldn't decode '%s' using override func: %w", fieldName, err)
-			}
-
-			eventFields[fieldName] = fieldValue
-
-			jsonPrint(fieldName, fieldValue)
-
-			continue
-		}
-
 		fieldTypeDef := fieldType.Def
 
-		switch {
-		case fieldTypeDef.IsComposite:
-			compositeFields, err := parseFields(meta, decoder, fieldTypeDef.Composite.Fields)
+		fieldValue, err := decodeTypeDef(meta, decoder, fieldTypeDef)
 
-			if err != nil {
-				return nil, fmt.Errorf("couldn't parse composite fields for %s: %w", fieldName, err)
-			}
-
-			eventFields[fieldName] = compositeFields
-
-			jsonPrint(fieldName, compositeFields)
-		case fieldTypeDef.IsVariant:
-			variantByte, err := decoder.ReadOneByte()
-
-			if err != nil {
-				return nil, fmt.Errorf("couldn't read variant byte for %s: %w", fieldName, err)
-			}
-
-			variantFound := false
-
-			for _, variant := range fieldTypeDef.Variant.Variants {
-				if byte(variant.Index) != variantByte {
-					continue
-				}
-
-				variantFound = true
-
-				if len(variant.Fields) == 0 {
-					eventFields[fieldName] = string(variant.Name)
-
-					jsonPrint(fieldName, string(variant.Name))
-
-					break
-				}
-
-				variantFields, err := parseFields(meta, decoder, variant.Fields)
-
-				if err != nil {
-					return nil, fmt.Errorf("couldn't parse variant fields for %s: %w", fieldName, err)
-				}
-
-				eventFields[fieldName] = variantFields
-
-				jsonPrint(fieldName, variantFields)
-			}
-
-			if !variantFound {
-				return nil, fmt.Errorf("variant %d not found for %s", variantByte, fieldName)
-			}
-		case fieldTypeDef.IsCompact:
-			compactFieldType, ok := meta.AsMetadataV14.EfficientLookup[fieldTypeDef.Compact.Type.Int64()]
-
-			if !ok {
-				return nil, fmt.Errorf("type not found for compact field %s", field.Name)
-			}
-
-			switch {
-			case compactFieldType.Def.IsPrimitive:
-				fieldValue, err := decode[types.UCompact](decoder)
-
-				if err != nil {
-					return nil, fmt.Errorf("couldn't decode primitive type for %s: %w", fieldName, err)
-				}
-
-				eventFields[fieldName] = fieldValue
-
-				jsonPrint(fieldName, fieldValue)
-			default:
-				return nil, fmt.Errorf("unsupported compact field type for %s", fieldName)
-			}
-		case fieldTypeDef.IsPrimitive:
-			primitiveValue, err := decodePrimitive(decoder, fieldTypeDef.Primitive.Si0TypeDefPrimitive)
-
-			if err != nil {
-				return nil, fmt.Errorf("couldn't decode primitive type for %s: %w", fieldName, err)
-			}
-
-			eventFields[fieldName] = primitiveValue
-
-			jsonPrint(fieldName, primitiveValue)
-		case fieldTypeDef.IsArray:
-			arrayFieldType, ok := meta.AsMetadataV14.EfficientLookup[fieldTypeDef.Array.Type.Int64()]
-
-			if !ok {
-				return nil, fmt.Errorf("type not found for array field '%s'", fieldName)
-			}
-
-			switch {
-			case arrayFieldType.Def.IsPrimitive:
-				arrayValue, err := decodePrimitiveArrayOfLength(uint32(fieldTypeDef.Array.Len), decoder, arrayFieldType.Def.Primitive.Si0TypeDefPrimitive)
-
-				if err != nil {
-					return nil, fmt.Errorf("couldn't get primitive slice for array field '%s': %w", fieldName, err)
-				}
-
-				eventFields[fieldName] = arrayValue
-
-				jsonPrint(fieldName, arrayValue)
-			default:
-				return nil, errors.New("unspupported array field type definition")
-			}
-		default:
-			return nil, errors.New("unsupported field type definition")
+		if err != nil {
+			return nil, fmt.Errorf("couldn't decode type definition for field '%s': %w", fieldName, err)
 		}
+
+		eventFields[fieldName] = fieldValue
 	}
 
 	return eventFields, nil
 }
 
-func jsonPrint(fieldName string, obj any) {
-	b, _ := json.Marshal(obj)
+func decodeTypeDef(meta *types.Metadata, decoder *scale.Decoder, typeDef types.Si1TypeDef) (any, error) {
+	switch {
+	case typeDef.IsCompact:
+		compactFieldType, ok := meta.AsMetadataV14.EfficientLookup[typeDef.Compact.Type.Int64()]
 
-	fmt.Printf("Field name - '%s': %s\n", fieldName, string(b))
+		if !ok {
+			return nil, errors.New("type not found for compact field")
+		}
+
+		return decodeCompact(meta, decoder, compactFieldType.Def)
+	case typeDef.IsComposite:
+		compositeFields, err := parseFields(meta, decoder, typeDef.Composite.Fields)
+
+		if err != nil {
+			return nil, fmt.Errorf("couldn't parse composite fields: %w", err)
+		}
+
+		return compositeFields, nil
+	case typeDef.IsVariant:
+		variantByte, err := decoder.ReadOneByte()
+
+		if err != nil {
+			return nil, fmt.Errorf("couldn't read variant byte: %w", err)
+		}
+
+		variantFound := false
+
+		for _, variant := range typeDef.Variant.Variants {
+			if byte(variant.Index) != variantByte {
+				continue
+			}
+
+			variantFound = true
+
+			if len(variant.Fields) == 0 {
+				return string(variant.Name), nil
+			}
+
+			variantFields, err := parseFields(meta, decoder, variant.Fields)
+
+			if err != nil {
+				return nil, fmt.Errorf("couldn't parse variant fields: %w", err)
+			}
+
+			return variantFields, nil
+		}
+
+		if !variantFound {
+			return nil, fmt.Errorf("variant %d not found", variantByte)
+		}
+	case typeDef.IsPrimitive:
+		primitiveValue, err := decodePrimitive(decoder, typeDef.Primitive.Si0TypeDefPrimitive)
+
+		if err != nil {
+			return nil, fmt.Errorf("couldn't decode primitive type: %w", err)
+		}
+
+		return primitiveValue, nil
+	case typeDef.IsArray:
+		arrayFieldType, ok := meta.AsMetadataV14.EfficientLookup[typeDef.Array.Type.Int64()]
+
+		if !ok {
+			return nil, fmt.Errorf("type not found for array field")
+		}
+
+		// TODO(cdamian): Cover all types.
+		switch {
+		case arrayFieldType.Def.IsPrimitive:
+			arrayValue, err := decodePrimitiveArrayOfLength(uint32(typeDef.Array.Len), decoder, arrayFieldType.Def.Primitive.Si0TypeDefPrimitive)
+
+			if err != nil {
+				return nil, fmt.Errorf("couldn't get primitive slice for array field: %w", err)
+			}
+
+			return arrayValue, nil
+		default:
+			return nil, errors.New("unsupported array field type definition")
+		}
+	case typeDef.IsSequence:
+		vectorFieldType, ok := meta.AsMetadataV14.EfficientLookup[typeDef.Sequence.Type.Int64()]
+
+		if !ok {
+			return nil, errors.New("type not found for vector field")
+		}
+
+		// TODO(cdamian): Cover all types.
+		switch {
+		case vectorFieldType.Def.IsPrimitive:
+			vectorValue, err := decodePrimitiveVector(decoder, vectorFieldType.Def.Primitive.Si0TypeDefPrimitive)
+
+			if err != nil {
+				return nil, fmt.Errorf("couldn't get primitive slice for vector field: %w", err)
+			}
+
+			return vectorValue, nil
+		case vectorFieldType.Def.IsTuple:
+			if vectorFieldType.Def.Tuple == nil {
+				return nil, nil
+			}
+
+			vectorLen, err := decoder.DecodeUintCompact()
+
+			if err != nil {
+				return nil, fmt.Errorf("couldn't decode vector length: %w", err)
+			}
+
+			var tupleVector []any
+
+			for i := uint64(0); i < vectorLen.Uint64(); i++ {
+				tuple, err := decodeTuple(meta, decoder, vectorFieldType.Def.Tuple)
+
+				if err != nil {
+					return nil, fmt.Errorf("couldn't decode vector tuple: %w", err)
+				}
+
+				tupleVector = append(tupleVector, tuple)
+			}
+
+			return tupleVector, nil
+		case vectorFieldType.Def.IsComposite:
+			vectorLen, err := decoder.DecodeUintCompact()
+
+			if err != nil {
+				return nil, fmt.Errorf("couldn't decode vector length: %w", err)
+			}
+
+			var compositeVector []any
+
+			for i := uint64(0); i < vectorLen.Uint64(); i++ {
+				compositeFields, err := parseFields(meta, decoder, vectorFieldType.Def.Composite.Fields)
+
+				if err != nil {
+					return nil, fmt.Errorf("couldn't parse composite vector fields: %w", err)
+				}
+
+				compositeVector = append(compositeVector, compositeFields)
+			}
+
+			return compositeVector, nil
+		default:
+			return nil, errors.New("unsupported vector field type definition")
+		}
+	case typeDef.IsTuple:
+		if typeDef.Tuple == nil {
+			return nil, nil
+		}
+
+		tuple, err := decodeTuple(meta, decoder, typeDef.Tuple)
+
+		if err != nil {
+			return nil, fmt.Errorf("couldn't decode touple: %w", err)
+		}
+
+		return tuple, nil
+	default:
+		return nil, errors.New("unsupported field type definition")
+	}
+
+	return nil, fmt.Errorf("unsupported type definition %v", typeDef)
+}
+
+func decodeCompact(meta *types.Metadata, decoder *scale.Decoder, typeDef types.Si1TypeDef) (any, error) {
+	// TODO(cdamian): Cover all types.
+	switch {
+	case typeDef.IsPrimitive:
+		// TODO(cdamian): Confirm that this covers all primitive types.
+		fieldValue, err := decode[types.UCompact](decoder)
+
+		if err != nil {
+			return nil, fmt.Errorf("couldn't decode primitive type: %w", err)
+		}
+
+		return fieldValue, nil
+	case typeDef.IsComposite:
+		compactCompositeFields := typeDef.Composite.Fields
+
+		compactCompositeMap := make(map[string]any)
+
+		for _, compactCompositeField := range compactCompositeFields {
+			compactCompositeFieldType, ok := meta.AsMetadataV14.EfficientLookup[compactCompositeField.Type.Int64()]
+
+			if !ok {
+				return nil, errors.New("compact composite field type not found")
+			}
+
+			compactComposite, err := decodeCompact(meta, decoder, compactCompositeFieldType.Def)
+
+			if err != nil {
+				return nil, fmt.Errorf("couldn't decode compact composite type: %w", err)
+			}
+
+			compactCompositeMap[string(compactCompositeField.Name)] = compactComposite
+		}
+
+		return compactCompositeMap, nil
+	default:
+		return nil, errors.New("unsupported compact field type")
+	}
+}
+
+func decodeTuple(meta *types.Metadata, decoder *scale.Decoder, tuple types.Si1TypeDefTuple) (any, error) {
+	var res []any
+
+	for _, item := range tuple {
+		// TODO(cdamian): Add a tuple item struct that has 2 fields for name (itemTypeDef.Path) and value?
+		itemTypeDef, ok := meta.AsMetadataV14.EfficientLookup[item.Int64()]
+
+		if !ok {
+			return nil, fmt.Errorf("type definition for tuple item %d not found", item.Int64())
+		}
+
+		tupleItem, err := decodeTypeDef(meta, decoder, itemTypeDef.Def)
+
+		if err != nil {
+			return nil, fmt.Errorf("couldn't decode tuple item: %w", err)
+		}
+
+		res = append(res, tupleItem)
+	}
+
+	return res, nil
+}
+
+func decodePrimitiveVector(decoder *scale.Decoder, primitiveTypeDef types.Si0TypeDefPrimitive) (any, error) {
+	switch primitiveTypeDef {
+	case types.IsBool:
+		return decodeVector[bool](decoder)
+	case types.IsChar:
+		return decodeVector[byte](decoder)
+	case types.IsStr:
+		return decodeVector[string](decoder)
+	case types.IsU8:
+		return decodeVector[types.U8](decoder)
+	case types.IsU16:
+		return decodeVector[types.U16](decoder)
+	case types.IsU32:
+		return decodeVector[types.U32](decoder)
+	case types.IsU64:
+		return decodeVector[types.U64](decoder)
+	case types.IsU128:
+		return decodeVector[types.U128](decoder)
+	case types.IsU256:
+		return decodeVector[types.U256](decoder)
+	case types.IsI8:
+		return decodeVector[types.I8](decoder)
+	case types.IsI16:
+		return decodeVector[types.I16](decoder)
+	case types.IsI32:
+		return decodeVector[types.I32](decoder)
+	case types.IsI64:
+		return decodeVector[types.I64](decoder)
+	case types.IsI128:
+		return decodeVector[types.I128](decoder)
+	case types.IsI256:
+		return decodeVector[types.I256](decoder)
+	}
+
+	return nil, fmt.Errorf("unsupported primitive type %v", primitiveTypeDef)
+}
+
+func decodeVector[T any](decoder *scale.Decoder) ([]T, error) {
+	var slice []T
+
+	if err := decoder.Decode(&slice); err != nil {
+		return nil, err
+	}
+
+	return slice, nil
 }
 
 func decodePrimitiveArrayOfLength(length uint32, decoder *scale.Decoder, primitiveTypeDef types.Si0TypeDefPrimitive) (any, error) {
